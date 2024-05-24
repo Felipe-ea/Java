11c42bc5f1d4c71e8f89d1060fa52dcf6b3c14a0
Felipe-ea <119639152+Felipe-ea@users.noreply.github.com>
2024-05-24 14:08:36 -0300

    Update Produto.java

--- produtosEmEstoque/src/entidades/Produto.java
+++ produtosEmEstoque/src/entidades/Produto.java
@@ -2,9 +2,31 @@ package entidades;
 
 public class Produto {
 	
-	public String nome;
-	public double preco;
-	public int quantidade;
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public Produto(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 	
 	public double totalValorEmEstoque() {
 		
